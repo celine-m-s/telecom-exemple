af3e7af7abc43b1d9b4d9ba15fa50a6ef033cfe4
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2015-10-22 17:34:58 +0200

    day-4

--- global.js
+++ global.js
@@ -1,4 +1,24 @@
 // Custom JS
 $(document).ready(function(){
-  // Add your code here
+  /* ==========================================================================
+     Simple Form - Contact Form
+     ========================================================================== */
+
+  $('#contact-form').submit(function(){
+    $.ajax({
+      dataType: 'jsonp',
+      url: "http://getsimpleform.com/messages/ajax?form_api_token=db43324d01c3a99e4a03fb9e0335d5c5",
+      data: $("#contactForm").serialize(),
+      error: function() {
+        $('#contactForm').hide();
+        $('#formErrorMessage').show();
+      }
+    }).done(function() {
+      //callback which can be used to show a thank you message
+      //and reset the form
+      $('#contactForm').hide();
+      $('#formSuccessMessage').show();
+    });
+    return false; //to stop the form from submitting
+  });
 });
